bf1aec7665caff1b92c11d2a2f7f118f5a9f72f5
arfat-xyz <arfatrahman08@gmail.com>
2023-02-16 19:01:26 +0600

    mobile added

--- frontend/components/CommingSoon.js
+++ frontend/components/CommingSoon.js
@@ -1,101 +1,45 @@
-import React, { useEffect, useRef, useState } from "react";
-// import useCountDown from "react-countdown-hook";
-const initialTime = 60 * 1000; // initial time in milliseconds, defaults to 60000
-const interval = 1000; // interval to change remaining time amount, defaults to 1000
-// const countDownDate =
-//   new Date("27 Nov 2023 23:01:22").getTime() - new Date().getTime();
+import { useCountdown } from "@/utils/countDownHook";
+import React from "react";
+
+import { MdLocationOn } from "react-icons/md";
 const CommingSoon = () => {
-  const Ref = useRef(null);
-  const [timer, setTimer] = useState("00:00:00");
-  const getTimeRemaining = (e) => {
-    const total = Date.parse(e) - Date.parse(new Date());
-    const seconds = Math.floor((total / 1000) % 60);
-    const minutes = Math.floor((total / 1000 / 60) % 60);
-    const hours = Math.floor((total / 1000 / 60 / 60) % 24);
-    return {
-      total,
-      hours,
-      minutes,
-      seconds,
-    };
-  };
-  const startTimer = (e) => {
-    let { total, hours, minutes, seconds } = getTimeRemaining(e);
-    if (total >= 0) {
-      // update the timer
-      // check if less than 10 then we need to
-      // add '0' at the beginning of the variable
-      setTimer(
-        (hours > 9 ? hours : "0" + hours) +
-          ":" +
-          (minutes > 9 ? minutes : "0" + minutes) +
-          ":" +
-          (seconds > 9 ? seconds : "0" + seconds)
-      );
-    }
-  };
-  const clearTimer = (e) => {
-    // If you adjust it you should also need to
-    // adjust the Endtime formula we are about
-    // to code next
-    setTimer("20:00:10");
-
-    // If you try to remove this line the
-    // updating of timer Variable will be
-    // after 1000ms or 1sec
-    if (Ref.current) clearInterval(Ref.current);
-    const id = setInterval(() => {
-      startTimer(e);
-    }, 1000);
-    Ref.current = id;
-  };
-  // const [timeLeft, { start, pause, resume, reset }] = useCountDown({
-  //   initialTime,
-  //   interval,
-  // });
-  const getDeadTime = () => {
-    let deadline = new Date();
-
-    // This is where you need to adjust if
-    // you entend to add more time
-    const temp = new Date("26 Nov, 2023 22:22:22").getTime() / 1000;
-    console.log(temp);
-    deadline.setSeconds(temp - deadline.getSeconds());
-    return deadline;
-  };
-  useEffect(() => {
-    clearTimer(getDeadTime());
-  }, []);
-
-  // Another way to call the clearTimer() to start
-  // the countdown is via action event from the
-  // button first we create function to be called
-  // by the button
-  const onClickReset = () => {
-    clearTimer(getDeadTime());
-  };
-
+  const time = new Date("22 Nov, 2023 22:22:22");
+  const timeLeft = useCountdown(time);
+  console.log(timeLeft);
   return (
     <>
-      {/* this is for md to large device  */}
       <div
-        className="w-100 hidden md:block  min-h-screen 
-        bg-[url('https://i.ibb.co/r0d6Ptd/Utshob-BD-bg.jpg')]
-         bg-cover 
-       "
+        className={`w-full bg-left-top h-screen md:bg-[url('/utshob-bg-image.jpeg')] 
+        bg-[url('/utshob-mobile-bg.jpg')] bg-cover bg-no-repeat`}
+        // style={{
+        //   backgroundImage: `url(${backgroundImage})`,
+        // }}
       >
-        <div className="grid grid-cols-2">
-          <div></div>
-          <div>
-            <div className="grid grid-cols-4">
-              <div className="col-span-2"></div>
-              <div>
-                <h1 className="text-6xl font-bold text-white col-span-2">
-                  Coming <br /> Soon!
-                </h1>
-                <div className="grid grid-cols-4 text-white text-2xl">
-                  <h2>{timer}</h2>
+        <div className=" w-10/12 mx-auto">
+          <div className="pt-9 h-screen flex justify-center items-center flex-col md:block">
+            <h1 className="md:text-right text-white text-7xl text-center lg:text-9xl md:text-6xl capitalize font-exo font-bold">
+              comming <br /> soon
+            </h1>
+            <div className="flex justify-end">
+              <div className="flex gap-[21px]">
+                <div className="w-[80px] font-bold text-[#6d6e70] bg-[#fbae17] rounded-xl flex justify-center items-center text-[27px] h-[48px]">
+                  {timeLeft[0]} D
                 </div>
+                <div className="w-[80px] font-bold text-[#6d6e70] bg-[#fbae17]  flex justify-center items-center rounded-xl text-[27px] h-[48px]">
+                  {timeLeft[1]} H
+                </div>
+                <div className="w-[80px] font-bold text-[#6d6e70] bg-[#fbae17]  flex justify-center items-center rounded-xl text-[27px] h-[48px]">
+                  {timeLeft[2]} M
+                </div>
+              </div>
+            </div>
+            <div className="md:h-[75%] lg:h-[50%] h-[55%] flex justify-end items-end">
+              <div className="flex text-[#fbae17] ">
+                <p className="capitalize font-bold font-exo text-2xl text-right">
+                  find us on <br />
+                  the map
+                </p>
+                <MdLocationOn size={70} />
               </div>
             </div>
           </div>
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,11 +1,9 @@
 import CommingSoon from "@/components/CommingSoon";
-import CommingSoon2 from "@/components/CommingSoon2";
 
 export default function Home() {
   return (
     <>
-      {/* <CommingSoon /> */}
-      <CommingSoon2 />
+      <CommingSoon />
     </>
   );
 }
